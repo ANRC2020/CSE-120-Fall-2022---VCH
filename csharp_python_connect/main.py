d1952166d3db0fcb0907221b2238920a2e52f41c
Shreya Shriram <shreyashriram@Shreyas-MacBook-Air.local>
2022-11-02 17:43:53 -0700

    Removed .exe

--- csharp_python_connect/main.py
+++ csharp_python_connect/main.py
@@ -3,9 +3,17 @@ import subprocess
 from subprocess import Popen, PIPE, STDOUT
 
 def main():
+
+    
+
+
     name = input("Name: ")
-    # subprocess.call(["/Users/shreyashriram/Documents/120/CSE-120-Fall-2022---VCH/csharp_python_connect/response.exe"])
+    id  = input("Patient ID")
+    
+
+
     os.system('"/Users/shreyashriram/Documents/120/CSE-120-Fall-2022---VCH/csharp_python_connect/response.exe"')
+    # send dict {name : name, id  : id }
 
 if __name__ == "__main__":
     main()

6119e1c87a2f1dd3a9590a268c1b14b516c04c78
Shreya Shriram <shreyashriram@ucmerced-10-33-48-160.ucmerced.edu>
2022-11-30 19:20:00 -0800

    added strength class function

--- DataExtractorAndDataBase/metric_utils.py
+++ DataExtractorAndDataBase/metric_utils.py
@@ -1,3 +1,9 @@
+# TODO: switch to config file
+# config variables
+healthy_buffer = 45
+significant_angle = 110
+sensing = True
+
 def getPatientId():
     return 1
 
@@ -7,7 +13,25 @@ def getAngle():
 def getVelocity():
     return 0
 
-def getStrengthClass():
+def getTime(position_array, velocity_array):
+    s = 0
+    return s
+
+def getStrengthClass(theta):
+
+    s = getTime()
+    rating = 0
+    
+    if (sensing):
+        rating = 2
+    
+    if (theta > significant_angle):
+        rating = rating+1
+
+    if (s >= 3):
+        rating = rating+1
+    
+    
     return 0
 
 

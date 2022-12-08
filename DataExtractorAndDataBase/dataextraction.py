906196d9370b90c295de41f1d3ef1563b84e9d52
Shreya Shriram <shreyashriram@ucmerced-10-33-48-160.ucmerced.edu>
2022-12-07 21:14:11 -0800

    data extraction pipeline skeleton finalized

--- DataExtractorAndDataBase/dataextraction.py
+++ DataExtractorAndDataBase/dataextraction.py
@@ -1,41 +1,56 @@
+import metric_utils as mu
+import util
+
+import sqlalchemy as db
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+
 from datetime import date
-import metric_utils
+import sys
 
-def exam_info():
+
+# TODO: DEBUG INSERT INTO DATABASE FUNCTION
+def insertintoDB():
+    # Connect to the database
+    engine = db.create_engine('sqlite:///VRDataBase.sqlite?check_same_thread=False')
+    conn = engine.connect()
+    metadata = db.MetaData()
+
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+
+    Exams = Base.classes.Exams
+
+    # query = db.insert(Exams).values(exam_ID = )
+    # conn.execute(query)
+
+
+def getMetrics(patient_measurements, positional_data):
+
+    l1, l2 = mu.armLength(patient_measurements)
+    angleList = mu.flexionAngleList(l1, l2, patient_measurements)
+
+    angle = mu.angleMetric(angleList)
+    velocity = mu.instVelocityList(angleList, positional_data)
+    time = mu.timeMetric(angleList, positional_data, angle)
+
+    strengthclass = mu.strengthClassMetric(angle, time)
+
+    return angle, velocity, strengthclass
+
+
+def main(patient_id):
+    # # TODO: REPLACE JSON FILEPATH ACCORDINGLY
+    patient_measurements_path = r"/Users/shreyashriram/Documents/120/CSE-120-Fall-2022---VCH/Angle and Angular Velocity/PositionData.json"
+    position_data_path = r"/Users/shreyashriram/Documents/120/CSE-120-Fall-2022---VCH/Angle and Angular Velocity/ObjectPositionData.json"
+
+    patient_measurements = util.parseJSON(patient_measurements_path)
+    position_data = util.parseJSON(position_data_path)
+
     exam_date = date.today()
-    patient_id = metric_utils.getPatientId()
-    task = 'A'
-    percentage_tasks_completed = 1
+    angle, velocity, strengthclass = getMetrics(patient_measurements, position_data)
 
-    return exam_date, patient_id, task, percentage_tasks_completed
-
-def exam_metrics():
-    angle_flexion = metric_utils.getAngle()
-    velocity_flexion = metric_utils.getVelocity(angle_flexion)
-    strength_class = metric_utils.getStrengthClass()
-
-    return angle_flexion, velocity_flexion, strength_class
-
-def main():
-    # GET .TXT FILE
-    # import arm_measurements txt file
-        # patient_name
-        # shoulder (x, y, z)
-        # elbow (x, y, z)
-        # palm (x, y, z)
-
-    # import  positional/velocity txt file
-        # velcities = [(x, y, z), (x, y, z), (x, y, z)]
-        # positions = [(x, y, z), (x, y, z), (x, y, z)]
-
-    a, b, c, d  = exam_info()
-    e, f, g = exam_metrics()
-
-    # insert a-g into database
-    
-
-    # return exam_id
-    
 
 if __name__ == "__main__":
-    main()
+    patient_id = sys.argv[1]
+    main(patient_id)
--- DataExtractorAndDataBase/metric_utils.py
+++ DataExtractorAndDataBase/metric_utils.py
@@ -1,36 +1,92 @@
-# TODO: switch to config file
-# config variables
-healthy_buffer = 45
-significant_angle = 110
-sensing = True
+def armLength(patient_measurements):
+    # This function returns the calculated forearm and arm measurements of the patient based on the three tagged positions. 
+    # Input: 
+    #   patient_measurements (dict): containing shoulder, elbow, palm (x, y, z) information 
+    # Output: 
+    #   forearm, arm (int) measurements based on euclidian distance
 
-def getPatientId():
-    return 1
-
-def getAngle():
-    return 0
-
-def getVelocity():
-    return 0
-
-def getTime(position_array, velocity_array):
-    s = 0
-    return s
-
-def getStrengthClass(theta):
-    s = getTime()
-    rating = 0
+    forearm = 0
+    arm = 0
     
-    if (sensing):
-        rating = 2
+    return forearm, arm
+
+
+def flexionAngleList(forearm, arm, positional_data):
+    # This function returns a list of angles for each time step in the positional_data list
+    # Input: 
+    #   forearm (int)
+    #   arm (int)
+    #   positional_data (dict): containing list (length = n) of positional data (x, y, z, t)
+
+    # Output: 
+    #   angleList (list, length = n-1): angles for each time step in the positional_data list
     
-    if (theta > significant_angle):
-        rating = rating+1
+    angleList = []
+    return  angleList
 
-    if (s >= 3):
-        rating = rating+1
+def instVelocityList(angleList, positional_data):
+    # This function returns calculated list of instantanious velcity for each time step in the positional_data list
+    # Input: 
+    #   angleList (list, length = n-1): angles for each time step in the positional_data list
+    #   positional_data (dict): containing list (length = n) of positional data (x, y, z, t)
+
+    # Output: 
+    #   velocityList (list, length = n-1): velocity for each time step in the positional_data list
     
+    velocityList = []
+    return  velocityList
 
-    return 0
+
+################################ METRIC EXTRACTION ################################
+
+def angleMetric(angleList):
+    # This function returns the minimum angle (between forearm and arm) the patient was able to make during the exam
+    # Input: 
+    #   angleList (list): angles for each time step in the positional_data list 
+    # Output: 
+    #   angleMetric (int): minimum angle
+  
+    angleMetric = min(angleList)
+    return angleMetric
+
+def timeMetric(angleList, positional_data, angleMetric):
+    # Function returns the amount of time a patient maintained the smallest angle flexion by finding the longest subset of the anglelist
+    # Input: 
+    #   angleList (list): angles for each time step in the positional_data list 
+    #   positional_data (dict): containing list (length = n) of positional data (x, y, z, t)
+    #   angleMetric (int): minimum angle
+
+    # Output: 
+    #   t (int): how long the minimum angle was maintained
+
+    t = 0
+    return t
+
+def velocityMetric(velocityList):
+    # This function returns the maximum velocity the patient was able to achieve during the exam
+    # Input: 
+    #   velocityList (list, length = n-1): velocity for each time step in the positional_data list
+    # Output: 
+    #   velocityMetric (int) : maximum velocity
+   
+    velocityMetric = max(velocityList) 
+    return velocityMetric
+
+def strengthClassMetric(angleMetric, time):
+    # This function returns the strength class metric [0, 5] based on the measured angle and time metric and classification scheme
+    # Input:
+    #   angleMetric (int): minimum angle
+    #   timemetric (int): maximum velocity
+    # Output: 
+    #   strengthClass (int): 
+
+    healthy_buffer = 45
+    significant_angle = 120
+    sensing = True
+    
+    strengthClass = 0
+    return strengthClass
 
 
+def dataVisualization(angleList):
+    # This function produces a gif animation of the angles made during the exam.
